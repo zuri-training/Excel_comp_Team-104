578bc1d8e50ee80ee9b4da0b899c6cee95ae52c7
KarmiTenchi <73952811+KarmiTenchi@users.noreply.github.com>
2022-08-14 02:20:07 +0300

    onboard commits

--- scripts.js
+++ scripts.js
@@ -3,3 +3,55 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+
+  const onboardingContainer = document.querySelector(".onboarding-container");
+const onboardingOverlay = document.querySelector(".onboarding-overlay");
+const skipBtn = document.querySelector(".onboarding-container .skip-btn");
+const steps = document.querySelectorAll(".onboarding-container .bottom-div");
+const stepsContainer = document.querySelector(".onboarding-container .bottom-divs");
+const nextBtn = document.querySelector(".next-btn");
+
+let stepPosition = 0;
+let currentStep = 0;
+
+
+const init = () => {
+  stepsContainer.style.transition = "unset";
+  onboardingContainer.classList.add("active");
+  onboardingOverlay.classList.add("active");
+  nextBtn.classList.add("active");
+  currentStep = 0;
+  stepPosition = 0;
+  stepsContainer.style.transform = `translateX(-${stepPosition}px)`;
+
+
+};
+
+if (onboardingContainer) {
+  init();
+}
+
+skipBtn.addEventListener("click", () => {
+  onboardingContainer.classList.remove("active");
+  onboardingOverlay.classList.remove("active");
+    nextBtn.classList.remove("active");
+});
+
+nextBtn.addEventListener("click", () => {
+  stepsContainer.style.transition = "all 400ms ease";
+  currentStep++;
+
+  if (currentStep >= steps.length) {
+    stepsContainer.style.transition = "unset";
+    onboardingContainer.classList.remove("active");
+    onboardingOverlay.classList.remove("active");
+    nextBtn.classList.remove("active");
+    currentStep = 0;
+  }
+
+  stepPosition += steps[0].offsetWidth;
+
+  stepsContainer.style.transform = `translateX(-${stepPosition * 1.61}px)`;
+
+});
+
