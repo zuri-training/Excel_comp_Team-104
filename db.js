8bfff364a79e3a36ce935972899337ed41982d6c
Nwagba195 <hnwagba@gmail.com>
2022-08-15 15:35:25 +0100

    db config updated

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql');
+const { Client } = require('pg');
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "************",
-    database: "group104",
-    port: "3306"
+const db = new Client({
+    host: "ec2-54-229-217-195.eu-west-1.compute.amazonaws.com",
+    user: "xnyekaqlmtlvhe",
+    password: "ee0848668a13c5708d18f6936128be7f46bc75dd5201631284fb4d6fce245e1d",
+    database: "d9kcg1k0j791ml",
+    port: "5432"
 });
 
 module.exports = db;
