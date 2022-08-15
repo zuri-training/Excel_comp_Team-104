5585cd67771a48fbe036f4ddbdd5c2dea1319a79
Nwagba195 <hnwagba@gmail.com>
2022-08-15 15:54:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -6,6 +6,9 @@ const db = new Client({
     password: "ee0848668a13c5708d18f6936128be7f46bc75dd5201631284fb4d6fce245e1d",
     database: "d9kcg1k0j791ml",
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db;
